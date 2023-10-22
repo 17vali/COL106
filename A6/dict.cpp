628dc88d344f006feaeade220922501727abfc97
17vali <rishiksaha2004@gmail.com>
2023-10-23 04:59:29 +0530

    dict done

---
+++ A6/dict.cpp
@@ -0,0 +1,78 @@
+// Do NOT add any other includes
+#include "dict.h"
+
+Dict::Dict(){
+    root = new TrieNode();
+}
+
+Dict::~Dict(){
+    delete root;
+}
+
+void Dict::insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence){
+    TrieNode* curr = root;
+    for(char w : sentence){
+        int index;
+        if(w>='a' && w<='z' ){
+            index = w-'a';
+        }
+        else if(w>='A' && w<='Z'){
+            index = w-'A';
+        }
+        else if(w>='0' && w<='9'){
+            index = w-'0'+26;
+        }
+        else{
+            string separators = " .,-:!\"\'()?—[]˙;@";
+            if(curr!=root && separators.find(w)!=string::npos){
+                curr->isEndOfWord = true;
+                curr->count++;
+                curr = root;
+            }
+            continue;
+        }
+        if(curr->children[index]==nullptr){
+            curr->children[index] = new TrieNode();
+        }
+        curr = curr->children[index];
+    }
+    if(curr!=root){
+        curr->isEndOfWord = true;
+        curr->count++;
+    }
+}
+
+int Dict::get_word_count(string word){
+    TrieNode* curr = root;
+    for(char w : word){
+        int index;
+        if(w>='a' && w<='z'){
+            index = w-'a';
+        }
+        else if(w>='A' && w<='Z'){
+            index = w-'A';
+        }
+        else if(w>='0' && w<='9'){
+            index = w-'0'+26;
+        }
+        else{
+            return 0;
+        }
+        if(curr->children[index]==nullptr){
+            return 0;
+        }
+        curr = curr->children[index];
+    }
+    if(curr->isEndOfWord){
+        return curr->count;
+    }
+    return 0;
+}
+
+void Dict::dump_dictionary(string filename){
+    ofstream file;
+    file.open(filename);
+    string word;
+    root->dump(file, word, root->count);
+    file.close();
+}
---
+++ A6/dict.h
@@ -0,0 +1,67 @@
+// Do NOT add any other includes
+#include <string> 
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class TrieNode{
+public:
+    vector<TrieNode*> children;
+    bool isEndOfWord;
+    int count;
+
+    TrieNode(){
+        children = vector<TrieNode*>(36, nullptr);
+        isEndOfWord = false;
+        count = 0;
+    }
+
+    ~TrieNode(){
+        for(int i=0;i<36;i++){
+            if(children[i]!=nullptr){
+                delete children[i];
+            }
+        }
+        delete this;
+    }
+
+    void dump(ofstream& file, string& word, int count){
+        if(isEndOfWord){
+            file<<word<<", "<<count<<endl;
+        }
+        for(int i=0;i<36;i++){
+            if(children[i]!=nullptr){
+                if(i<26){
+                    word.push_back('a'+i);
+                }
+                else{
+                    word.push_back('0'+i-26);
+                }
+                children[i]->dump(file, word, children[i]->count);
+                word.pop_back();
+            }
+        }
+    }
+};
+
+class Dict {
+private:
+    TrieNode* root;
+
+public: 
+    /* Please do not touch the attributes and 
+    functions within the guard lines placed below  */
+    /* ------------------------------------------- */
+    Dict();
+
+    ~Dict();
+
+    void insert_sentence(int book_code, int page, int paragraph, int sentence_no, string sentence);
+
+    int get_word_count(string word);
+
+    void dump_dictionary(string filename);
+
+    /* -----------------------------------------*/
+};
